e36b79662a1c1cbd7df3481506e1bfde6fa4506c
jarwix <oazisrus@gmail.com>
2020-10-07 02:45:45 +0300

    Update Easy 1470 Shuffle the Array.c

--- Easy 1470 Shuffle the Array.c
+++ Easy 1470 Shuffle the Array.c
@@ -1,1 +1,18 @@
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize)
+{
+    int     i;
+    int     *result;
+    
+    i = -1;
+    result = malloc(sizeof(int) * numsSize);
+    while(++i < n)
+    {
+        result[i * 2] = nums[i];
+        result[i * 2 + 1] = nums[n + i];
+    }
+    *returnSize = numsSize;
+    return result;
+}
