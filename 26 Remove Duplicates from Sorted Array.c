6d36acd919ef9e83cb890e8153a5eb84f9707c70
jarwix <oazisrus@gmail.com>
2021-03-07 22:57:01 +0500

    Create 26 Remove Duplicates from Sorted Array.c

---
+++ 26 Remove Duplicates from Sorted Array.c
@@ -0,0 +1,24 @@
+int removeDuplicates(int* nums, int numsSize)
+{
+    int last;
+    int i;
+    int returnNum;
+
+    i = 0;
+    if (numsSize != 0)
+    {
+        last = nums[0];
+        returnNum = 1;
+    }
+    else
+        returnNum = 0;
+    while(++i < numsSize)
+    {
+        if(nums[i] != nums[i - 1])
+        {
+            ++returnNum;
+            nums[returnNum - 1] = nums[i];
+        }
+    }
+    return returnNum;
+}
